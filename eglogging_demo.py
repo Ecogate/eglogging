7fd45a7fef740a5048ccda573bab4eb0d05e0e36
Krystof Litomisky <krystof@litomisky.com>
2020-03-27 11:38:48 -0700

    Added an alternative, more human-friendly log format

--- eglogging_demo.py
+++ eglogging_demo.py
@@ -11,8 +11,9 @@ if __name__ == '__main__':
     INFO('hello')
     WARN("Winter is coming.")
 
-    logging_load_config_from_file('logger_config_ALTERNATE.json')
-    DEBUG("is this alternate?")
+    DEBUG("Loading a more human-friendly config")
+    logging_load_config_from_file('logger_config_HUMAN.json')
+    ERROR("This isn't an error, but it should easier to read")
 
   except Exception as ex:
     MSG("Exception: {}".format(ex))
